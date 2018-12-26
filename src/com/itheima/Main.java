20a1fb431fbe3471992958432999e04383580d2d
948170684 <Adisheng_parroot@163.com>
2018-12-26 20:50:32 +0800

    添加test工程至git第一次

---
+++ src/com/itheima/Main.java
@@ -0,0 +1,12 @@
+package com.itheima;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("this is git project");
+        System.out.println("this is git project");
+        System.out.println("this is git project");
+        System.out.println("this is git project");
+        System.out.println("this is git project");
+    }
+}
